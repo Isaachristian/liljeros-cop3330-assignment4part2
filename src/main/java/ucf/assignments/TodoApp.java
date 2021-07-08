d317104846f9a6d695f2caf0f48e4c92f2d9dcbb
Isaac Liljeros <isaak.liljeros@gmail.com>
2021-07-08 09:46:45 -0400

    Got UI Running

--- src/main/java/ucf/assignments/TodoApp.java
+++ src/main/java/ucf/assignments/TodoApp.java
@@ -6,6 +6,9 @@
 package ucf.assignments;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class TodoApp extends Application {
@@ -13,9 +16,11 @@ public class TodoApp extends Application {
     public static void main(String[] args) { launch(args); }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // load in fxml file
-        // set as scene
-        // start the app
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(ClassLoader.getSystemResource("App.fxml"));
+        var scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
